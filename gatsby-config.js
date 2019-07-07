d79d2c748cf98d3bdf4b9ed4a1d68c7af237e455
McKenzie Fiege <mckenziefiege@McKenzies-MBP.home>
2019-07-07 12:20:52 -0400

    updated localstorage

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 module.exports = {
+  pathPrefix: "/arfa-assessment",
   siteMetadata: {
     title: `Arfa Candidate Assessment`,
     description: ``,
